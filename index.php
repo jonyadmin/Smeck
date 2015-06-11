719d6e69f4955ae3715e2a06e585efac50fb3466
jonyadmin <jonnyabas.contato@gmail.com>
2015-06-11 18:04:47 -0300

    Inicio do projeto
    inicio Index

---
+++ index.php
@@ -0,0 +1,14 @@
+<html><!-- Inicio do cogido da pagina index em HTML -->
+
+	<head><!-- Inicio do cogido do Head pagina index em HTML -->
+	<title>Bem-vindo ao Smeck - Acesse!</title>
+	<!--<link rel="shortcut icon" href="favicon.png" type="image/png" />
+    <link rel="stylesheet" href="layout/style.css" type="text/css" />
+	<script type="text/javascript" src="js/jquery-1.3.2.min.js"></script>-->
+	</head><!-- FIM do cogido do Head pagina index em HTML -->
+	
+	<body><!-- Inicio do cogido do Body pagina index em HTML -->
+	
+	</body><!-- Fim do cogido do Body pagina index em HTML -->
+
+</html><!-- Fim do cogido da pagina index em HTML -->
